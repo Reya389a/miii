3fbc023fb98f71d22b63d7d93e72da9726b1b396
Reya389a <108363628+Reya389a@users.noreply.github.com>
2023-01-26 21:15:27 -0500

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,24 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.image.fromURL('Birthdayimage.jpg' , function(img) {
+    block_image_object - img;
+
+    block_image_object.scaleToWidth(700);
+    block_image_object.scaleToWidth(150);
+    block_image_object.set({
+        top:0,
+        left:0
+    });
+    canvas.add(block_image_object);
+});
+    
+}	
+	
+
+
+function playSound(){
+	x.play();
+}
